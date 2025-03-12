9f69060fa9c2da012e93b726df2979f2841f27c6
Bpost129 <Bpost129@yahoo.com>
2025-03-11 22:07:09 -0400

    remove default stylings, style basic navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,10 +2,10 @@ import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <>
+    <main id='navbar'>
       <NavLink to='/'>Home</NavLink>
       <NavLink to='/players'>Players</NavLink>
-    </>
+    </main>
   )
 }
 
