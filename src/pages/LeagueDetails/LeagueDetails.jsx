9ef3e769b2d9cbc9fdb6e6503058e85fceb856ef
Bpost129 <Bpost129@yahoo.com>
2025-03-22 00:05:02 -0400

    render list of league members

--- src/pages/LeagueDetails/LeagueDetails.jsx
+++ src/pages/LeagueDetails/LeagueDetails.jsx
@@ -1,16 +1,20 @@
 import { useState, useEffect } from "react"
 import { useParams, NavLink } from "react-router-dom"
 
-import { getLeague } from "../../services/sleeper-api"
+import { getLeague, getLeagueUsers } from "../../services/sleeper-api"
 
 const LeagueDetails = () => {
   const [league, setLeague] = useState({})
+  const [users, setUsers] = useState([])
   const { leagueId } = useParams()
 
   useEffect(() => {
     const fetchLeagueDetails = async () => {
       const leagueData = await getLeague(leagueId)
+      const usersData = await getLeagueUsers(leagueId)
       setLeague(leagueData)
+      setUsers(usersData)
+      console.log(usersData)
     }
     fetchLeagueDetails()
   }, [leagueId])
@@ -26,10 +30,16 @@ const LeagueDetails = () => {
         <h1>Details</h1>
         <img src={`https://sleepercdn.com/avatars/thumbs/${league.avatar}`} alt="" />
         <p>{league.name}</p>
-        <p>Season: {league.season}</p>
-        <p>Teams: {league.total_rosters}</p>
-        <p>Status: {league.status}</p>
-        <p></p>
+        <p>SEASON: {league.season}</p>
+        <p>TEAMS: {league.total_rosters}</p>
+        <p>STATUS: {league.status}</p>
+
+        <ul >
+          MEMBERS
+          {users.map((user, idx) => 
+            <li key={idx}>{user.display_name}</li>
+          )}
+        </ul>
       </main>
     </>
   )
