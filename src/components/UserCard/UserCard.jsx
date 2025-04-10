7649ed0116bb683c2a1e247aa3b1417b98dcf67a
Bpost129 <Bpost129@yahoo.com>
2025-04-09 22:00:14 -0400

    conditionally render avatar on user page instead of rendering empty image, add border to roster table and highlight headers and columns

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,7 +1,8 @@
 const UserCard = ({ user }) => {
   return (
     <div className="user-card">
-      <img src={`https://sleepercdn.com/avatars/thumbs/${user.avatar}`} alt="" />
+      {user.avatar && 
+      <img src={`https://sleepercdn.com/avatars/thumbs/${user.avatar}`} alt="" />}
       <p>Username: {user.username}</p>
       <p>Display Name: {user.display_name}</p>
       <p>User ID: {user.user_id}</p>
